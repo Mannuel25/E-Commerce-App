8aaa68fc3b8d1375c1d2e55d72a6ce49586a7019
mannuel25 <oluwasegunprosperity@gmail.com>
2023-05-12 14:38:31 +0100

    update

--- users/views.py
+++ users/views.py
@@ -25,7 +25,7 @@ def signin(request):
         user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
-            return redirect('home')
+            return redirect('dashboard')
     return render(request, 'login.html')
 
 
