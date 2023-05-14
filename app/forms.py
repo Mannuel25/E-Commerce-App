af79fc39ae422a5bf2195ea22a0949e81ac4fa69
mannuel25 <oluwasegunprosperity@gmail.com>
2023-05-14 13:28:58 +0100

    update

--- app/forms.py
+++ app/forms.py
@@ -2,29 +2,29 @@ from django import forms
 from .models import Clothings, PhoneAndAccessories, HomeAndOffice, HealthAndBeauty, Gaming
 
 
-class ClothingsForm(UserChangeForm):
+class ClothingsForm(forms.ModelForm):
     class Meta:
         fields = '__all__'
         model = Clothings
 
-class PhoneAndAccessoriesForm(UserChangeForm):
+class PhoneAndAccessoriesForm(forms.ModelForm):
     class Meta:
         fields = '__all__'
         model = PhoneAndAccessories
 
 
-class HomeAndOfficeForm(UserChangeForm):
+class HomeAndOfficeForm(forms.ModelForm):
     class Meta:
         fields = '__all__'
         model = HomeAndOffice
 
-class HealthAndBeautyForm(UserChangeForm):
+class HealthAndBeautyForm(forms.ModelForm):
     class Meta:
         fields = '__all__'
         model = HealthAndBeauty
 
 
-class GamingForm(UserChangeForm):
+class GamingForm(forms.ModelForm):
     class Meta:
         fields = '__all__'
         model = Gaming
